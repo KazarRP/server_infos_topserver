bc833824ab91c1d07254e4d5e48d95876b624918
Jago7480 <alexandre.riquez@gmail.com>
2023-02-27 13:10:02 +0100

    fetch

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ $(document).ready(function() {
 	var api_url = "https://api.top-serveurs.net/v1/servers/" + server_token;
 
 	// Fetch server information from API and update page content
-	$.getJSON(api_url, function(data) {
-		$("#server_name").text(data.name);
-		$("#server_description").text(data.short_description);
-		$("#server_address").text(data.address);
-		$("#server_port").text(data.port);
-		$("#server_version").text(data.version);
-	});
+	fetch(api_url)
+		.then(response => response.json())
+		.then(data => {
+			$("#server_name").text(data.name);
+			$("#server_description").text(data.short_description);
+			$("#server_address").text(data.address);
+			$("#server_port").text(data.port);
+			$("#server_version").text(data.version);
+		})
+		.catch(error => console.log(error));
 });
